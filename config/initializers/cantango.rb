cce3b0ce71e1aeb5f42ec2a9014633cd80eccd4c
Kristian Mandrup <kmandrup@gmail.com>
2011-11-07 20:51:07 +0100

    nil problem in executor if no rules found for subject

--- config/initializers/cantango.rb
+++ config/initializers/cantango.rb
@@ -3,6 +3,7 @@ CanTango.config do |config|
 
   config.engine(:permission).set :off
   config.engine(:permit).set :on
+  config.ability.mode = :no_cache
   # config.enable_helpers :rest
 
   # config.guest.user lambda { Guest.new }
