24fda0812a50a0a79c567ac2fdab8cf526d4ee97
Kristian Mandrup <kmandrup@gmail.com>
2011-11-07 20:55:28 +0100

    fixed bugs

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register AdminUser do
 
   menu :if => proc{ admin_can?(:manage, AdminUser) }
 
-  if CanTango::Api.current_user_ability(:admin).can?( :change_password, resource )
+  if CanTango::Api.current_user_ability(:admin).can?( :change_password, AdminUser )
     action_item :only => [:show, :edit] do
       link_to "Change Password", change_password_app_admin_user_path( resource )
     end
